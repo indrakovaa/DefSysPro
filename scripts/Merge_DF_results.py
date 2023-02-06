6b074a401f92f79b39c7067cd4f0933129573cf1
Adela Finstrlova <adela.finstrlova@gmail.com>
2023-02-06 09:47:32 +0100

    Merge all defFinder genes.tsv results

--- scripts/Merge_DF_results.py
+++ scripts/Merge_DF_results.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 import pandas as pd
 import os
+import pathlib
 
-all_DF_res = pd.DataFrame()
-
-# open desired file, append to new DF
+# list of desired files
+FILES = []
 for root, dirs, files in os.walk("DefFinder_results/"):
     for file in files:
         if file.endswith('genes.tsv'):
-            data = pd.read_csv(file, sep='\t')
-            all_DF_res = all_DF_res.append(data)
+            FILES.append(os.path.join(root, file))
+            
 
-all_DF_res.to_csv('DF_result.tsv', sep='\t', index=False)
+# read all data frames
+dfs = [pd.read_csv(file, sep='\t') for file in FILES]
+# create one table
+all_DF_res = pd.concat(dfs)
+
+
+all_DF_res.to_csv('DF_result.tsv', sep='\t', header=True, index=False)
+# Jeste bych potrebovala nazev slozky - NC_001416 pridat do prvniho sloupce
