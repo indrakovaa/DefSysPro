bfd34878b1eb436235a9d1613eded401dbaa990f
Adela Finstrlova <adela.finstrlova@gmail.com>
2023-02-06 15:56:30 +0100

    Rough way to analyze results from RefSeq

--- scripts/Merge_DF_results.py
+++ scripts/Merge_DF_results.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import pandas as pd
 import os
-import pathlib
 
 # list of desired files
 FILES = []
@@ -15,7 +14,6 @@ for root, dirs, files in os.walk("DefFinder_results/"):
 dfs = [pd.read_csv(file, sep='\t') for file in FILES]
 # create one table
 all_DF_res = pd.concat(dfs)
-
 
 all_DF_res.to_csv('DF_result.tsv', sep='\t', header=True, index=False)
 # Jeste bych potrebovala nazev slozky - NC_001416 pridat do prvniho sloupce
---
+++ scripts/Print_fasta_headers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+from Bio import SeqIO
+import numpy as np
+
+# read list of directories with results
+fasta_dirs =  []
+
+# Open the file and read the content in a list ## funguje
+with open('DF_phi_list.txt', 'r') as filehandle:
+    directories = filehandle.readlines()
+    for line in directories:
+        # Remove linebreak which is the last character of the string
+        fasta_dir = line[:-1]
+        # Add item to the list
+        fasta_dirs.append(fasta_dir)
+
+print(fasta_dirs)
+
+# print headers for each fasta_dir ## funguje
+root_path = "/home/ca47yas/DefSystems/Phage_genomes/Refseq/single_refseq_genomes/GBid/"
+headers = []
+for item in fasta_dirs:
+    path = os.path.join(root_path, item, item + ".fasta")
+    with open(path, "r") as f:
+        for record in SeqIO.parse(f, "fasta"):
+            headers.append(record.description)
+
+np.savetxt("Phages_with_DF.csv", 
+           headers,
+           delimiter =", ", 
+           fmt ='% s')
